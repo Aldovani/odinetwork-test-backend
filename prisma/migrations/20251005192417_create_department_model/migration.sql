cee24e2d08b3eff0ca64717c1ae82498224febf0
Aldovani <aldovanihcosta@gmail.com>
2025-10-05 21:34:54 -0300

    feat: add Department model with migration and schema updates for PostgreSQL

---
+++ prisma/migrations/20251005192417_create_department_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "department" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "department_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "department_name_key" ON "department"("name");
