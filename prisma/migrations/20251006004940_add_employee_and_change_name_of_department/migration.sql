f31c8a218a0d77d0145a94e9eb07daf82b8b7eab
Aldovani <aldovanihcosta@gmail.com>
2025-10-06 11:58:04 -0300

    feat: add Employee entity with properties, methods, and relationships to Department

---
+++ prisma/migrations/20251006004940_add_employee_and_change_name_of_department/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the `department` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."department";
+
+-- CreateTable
+CREATE TABLE "departments" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "departments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "employees" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "department_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "departments_name_key" ON "departments"("name");
+
+-- AddForeignKey
+ALTER TABLE "employees" ADD CONSTRAINT "employees_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "departments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/commons/entities/employee.ts
@@ -0,0 +1,67 @@
+import { Department } from './department'
+import { Entity } from './entity'
+
+export type EmployeeProps = {
+  name: string
+  email: string
+  departmentId: number
+  department: Department
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export class Employee extends Entity<EmployeeProps, number> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get departmentId() {
+    return this.props.departmentId
+  }
+
+  get createdAt() {
+    return this.props?.createdAt
+  }
+
+  get updatedAt() {
+    return this.props?.updatedAt
+  }
+
+  get department() {
+    return this.props.department
+  }
+
+  set name(name: string) {
+    this.props.name = name
+  }
+
+  set email(email: string) {
+    this.props.email = email
+  }
+
+  set department(department: Department) {
+    this.props.department = department
+  }
+
+  set departmentId(departmentId: number) {
+    this.props.departmentId = departmentId
+  }
+
+  set createdAt(createdAt: Date | undefined) {
+    this.props.createdAt = createdAt
+  }
+
+  set updatedAt(updatedAt: Date | undefined) {
+    this.props.updatedAt = updatedAt
+  }
+
+  static create(props: EmployeeProps, id?: number) {
+    const employee = new Employee(props, id)
+
+    return employee
+  }
+}
